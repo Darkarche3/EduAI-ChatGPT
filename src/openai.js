91f3f55d66a988f9fa4f3fdefc4f4bd4ef26d598
Brendan8899 <80048882+Brendan8899@users.noreply.github.com>
2023-10-21 14:32:08 +0800

    Update openai.js

--- src/openai.js
+++ src/openai.js
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 const openai = new OpenAI({
-    apiKey: "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", dangerouslyAllowBrowser: true
+    apiKey: process.env.API_URL;, dangerouslyAllowBrowser: true
   });
 //const { Configuration, OpenAIApi } = require('openai');
 //const configuration= new Configuration({ apiKey: "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"});
