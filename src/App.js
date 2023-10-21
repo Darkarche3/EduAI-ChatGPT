75a32c4eb3a83b98bfa7471665eaf0983832c0f3
Brendan8899 <80048882+Brendan8899@users.noreply.github.com>
2023-10-21 12:20:05 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ import { useEffect, useRef, useState } from 'react';
 function App() {
   const msgEnd = useRef(null);
   const[input, setInput] = useState("");
+  const[knowledgelevel, setKnowledgeLevel] = useState("");
+  const[levelandTopic, setLevelandTopic] = useState(true);
+  const[practiceQuestionsTriggered, setTrigger] = useState(true);
+  const[firsttime, setFirstTime] = useState(true);
   const [messages, setMessages] = useState([{
     text: "Hi I am EduAI, designed to help you study, what would you like to study today?",
     isBot: true,
@@ -24,19 +28,53 @@ function App() {
     msgEnd.current.scrollIntoView();
   },[messages]);
 
+
   const handleSend = async() => {
     const text = input;
     setInput('');
     setMessages([
       ...messages,
       {text, isBot: false}
-    ])
-    const res = await sendMsgToOpenAI(text);
+    ]);
+    if (levelandTopic) {
+      setMessages([
+        ...messages,
+        { text, isBot: false},
+        {
+          text: "Are you studying IB or 'A' Level? Which Topic would you like to learn?",
+          isBot: true,
+        },
+      ]);
+      setLevelandTopic(false);
+    } else if (!levelandTopic && firsttime) {
+    setKnowledgeLevel(text);
+    const text2 = text + "Can you provide me with the content required as well as step by step instructions on how to study this topic?"
+    const res = await sendMsgToOpenAI(text2);
     setMessages([...messages,
       { text, isBot: false},
-      { text : "What is your level of knowledge on this subject? Beginner, Intermediate or Advanced?", isBot: true}])
-      console.log(messages)
+      { text: res, isBot: true},
+      { text: "Would you like a list of practice questions?", isBot: true}])
+      setFirstTime(false);
+    } else {
+      if (text.includes("ye") && practiceQuestionsTriggered) {
+        const newString = "Let's start a Q&A Session. Ask me a practice question regarding " + knowledgelevel
+        console.log(newString)
+        const res = await sendMsgToOpenAI(newString);
+        setMessages([...messages,
+          { text, isBot: false},
+          { text : res, isBot: true}]);
+        }
+      else {
+        const res = await sendMsgToOpenAI(text + "Tell me if I am correct. What's your next question?");
+        setMessages([...messages,
+          { text, isBot: false},
+          { text : res, isBot: true}]);
+      }
+      setTrigger(false);
+    }
   }
+
+  console.log(knowledgelevel);
 
   const handleEnter = async (e) => {
     if (e.key ==='Enter') await handleSend();
@@ -76,7 +114,7 @@ function App() {
   <div className="main">
     <div className="chats">
       {messages.map((message, i) => 
-        <div key={1} className={message.isBot?"chat bot":"chat"}>
+        <div key={i} className={message.isBot?"chat bot":"chat"}>
           <img className='chatImg' src={message.isBot?gptImgLogo:userIcon} alt="" /><p className="txt">{message.text}</p>
         </div>
       )}
